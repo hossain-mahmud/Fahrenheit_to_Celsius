4ee8713b4944580e09ef464a83072250da900014
Hossain Mahmud <hossainmahmud408@gmail.com>
2023-10-03 18:31:20 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let input_ahrenheit = document.querySelector('#input_ahrenheit');
 let calculate = document.querySelector('#calculate');
 let celsius_output = document.querySelector('#celsius_output');
 calculate = addEventListener('click', function () {
-    celsius_output.innerHTML = (input_ahrenheit.value - 32) / (9 / 5) + '°' + ' Calculate '
+    celsius_output.innerHTML = (input_ahrenheit.value - 32) / (9 / 5) + '°' + ' Celsius '
     if (input_ahrenheit.value <= 0 || isNaN(input_ahrenheit.value)) {
         celsius_output.innerHTML = 'Please Input Valid Fahrenheit !'
     }
