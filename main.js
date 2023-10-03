ac1523e560aea3c683b86baf41245572e3fa00a0
Hossain Mahmud <hossainmahmud408@gmail.com>
2023-10-03 17:50:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let input_ahrenheit = document.querySelector('#input_ahrenheit');
+let calculate = document.querySelector('#calculate');
+let celsius_output = document.querySelector('#celsius_output');
+calculate = addEventListener('click', function () {
+    celsius_output.innerHTML = (input_ahrenheit.value - 32) / (9 / 5) + '°' + ' Calculate '
+    if (input_ahrenheit.value <= 0 || isNaN(input_ahrenheit.value)) {
+        celsius_output.innerHTML = 'Please Input Valid Fahrenheit !'
+    }
+})
